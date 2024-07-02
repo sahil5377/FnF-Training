464c3a00e19260b3cf2aec3fb9d1442760f1489d
sahil5377 <161150106+sahil5377@users.noreply.github.com>
2024-07-02 12:23:41 +0530

    Update ExampleDelegateSearch.cs

--- training/training/ExampleDelegateSearch.cs
+++ training/training/ExampleDelegateSearch.cs
@@ -1,30 +1,50 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
 
 namespace Sample
 {
     class DelegatesSearch
     {
-        List<string> items = new List<string>
+        static void Main()
         {
-            "Apples","Bannana", "Mangoes","Tomatoes", "Grapes", "Rin","Soaps"
-        };
-        Console.WriteLine("Enter the name of the item to find");
-        string selected = Console.ReadLine();
+            List<string> items = new List<string>
+            {
+                "Apples", "Banana", "Mangoes", "Tomatoes", "Grapes", "Rin", "Soaps"
+            };
 
-        var found = items.Find(delegate (string arg)
-        {
-            return arg.Contains(selected);
-        });
+            Console.WriteLine("Enter the name of the item to find:");
+            string selected = Console.ReadLine();
 
-        var selectedItems = items.FindAll(item => item.Contains(selected));
-        Console.WriteLine("There are other items with matching letters");
-        foreach (var item in selectedItems){
-            Console.WriteLine(item);}
-}
+            // Using Find method with delegate
+            var found = items.Find(delegate (string arg)
+            {
+                return arg.Contains(selected);
+            });
+
+            if (found != null)
+            {
+                Console.WriteLine("Found item: " + found);
+            }
+            else
+            {
+                Console.WriteLine("Item not found.");
+            }
+
+            // Using FindAll method with lambda expression
+            var selectedItems = items.FindAll(item => item.Contains(selected));
+            if (selectedItems.Count > 0)
+            {
+                Console.WriteLine("Other items with matching letters:");
+                foreach (var item in selectedItems)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No other items with matching letters found.");
+            }
+        }
     }
 }
