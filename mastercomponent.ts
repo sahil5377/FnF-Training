46554ccfde0c83b92710077bed59134fa3846361
sahil5377 <161150106+sahil5377@users.noreply.github.com>
2024-08-08 13:45:30 +0530

    Update mastercomponent.ts

--- mastercomponent.ts
+++ mastercomponent.ts
@@ -6,19 +6,25 @@ import { Employee } from '../../Models/employee';
   templateUrl: './master.component.html',
 })
 export class MasterComponent implements OnInit {
+  empList: Employee[] = [];
+
   ngOnInit(): void {
-    this.empList.push({empId : 123,empName : "Sahil",empAddress: "Bangalore",empSalary: 45000,empPic:"img1.jpg"});
-    this.empList.push({empId : 124,empName : "Sakshi",empAddress: "Hubali",empSalary: 40000,empPic:"img2.jpg"});
-    this.empList.push({empId : 125,empName : "Arpita",empAddress: "Belagavi",empSalary: 55000,empPic:"img3.jpg"});
-    this.empList.push({empId : 126,empName : "Chaita",empAddress: "Gokarna",empSalary: 50000,empPic:"img4.jpg"});
+    this.empList.push(
+      { empId: 123, empName: "Sahil", empAddress: "Bangalore", empSalary: 45000, empPic: "img1.jpg" },
+      { empId: 124, empName: "Sakshi", empAddress: "Hubali", empSalary: 40000, empPic: "img2.jpg" },
+      { empId: 125, empName: "Arpita", empAddress: "Belagavi", empSalary: 55000, empPic: "img3.jpg" },
+      { empId: 126, empName: "Chaita", empAddress: "Gokarna", empSalary: 50000, empPic: "img4.jpg" }
+    );
   }
-onEdit = (rec : number)=>{
 
-};
+  onEdit = (rec: number) => {
+    // Implement edit functionality here
+  };
 
-onDelete = (emp: Employee) => {
-  const index = this.empList.indexOf(emp);
-  this.empList.splice(index, 1);
-};
-  empList: Employee[]=[]
+  onDelete = (emp: Employee) => {
+    const index = this.empList.indexOf(emp);
+    if (index > -1) {
+      this.empList.splice(index, 1);
+    }
+  };
 }
