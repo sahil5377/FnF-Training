f37bbe475dfb006e1e1f55ccfa79ce719e57d94c
sahil5377 <161150106+sahil5377@users.noreply.github.com>
2024-08-20 11:07:15 +0530

    Update HomeController.cs

--- Project_Medical_Insurance/HomeController.cs
+++ Project_Medical_Insurance/HomeController.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,19 +23,10 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult Dashboard()
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            return View();
-        }
-
-        public IActionResult Policies()
-        {
-            return View();
-        }
-
-        public IActionResult Claims()
-        {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
