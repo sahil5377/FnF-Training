c6009316e9292571108cf5368d0e2993554424e4
sahil5377 <161150106+sahil5377@users.noreply.github.com>
2024-08-20 11:06:49 +0530

    Create HomeController.cs

---
+++ Project_Medical_Insurance/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Policies()
+        {
+            return View();
+        }
+
+        public IActionResult Claims()
+        {
+            return View();
+        }
+    }
+}
